b8b55bbb353dfd041e25ed8e72c169fae428d71c
Igor Gorodilov <49714872+Qyaers@users.noreply.github.com>
2024-09-04 16:25:45 +0400

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,128 @@
+// ------------------------------- variables ------------------------------
+
+const quoteURL = "https://type.fit/api/quotes";
+
+// ------------------------------- functions ------------------------------
+const changeBackGround = () => {
+	let backgroundImageArr = [
+		'/assets/img/1.jpg',
+		'/assets/img/2.jpg',
+		'/assets/img/3.jpg',
+		'/assets/img/4.jpg',
+		'/assets/img/5.jpg',
+		'/assets/img/6.jpg'];
+	let randomNum = getRandomIntInclusive(0, backgroundImageArr.length);
+	do {
+		randomNum = getRandomIntInclusive(0, backgroundImageArr.length);
+	} while (document.body.style.backgroundImage == `url("${backgroundImageArr[randomNum]}")`)
+	document.body.style.backgroundImage = `url(${backgroundImageArr[randomNum]})`;
+
+}
+
+const getRandomIntInclusive = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const getTime = () => {
+	let now = new Date();
+	let timeNow = now.getHours() + ":" + (now.getMinutes() > 9 ? now.getMinutes() : "0" + now.getMinutes()) + ":" + (now.getSeconds() > 9 ? now.getSeconds() : "0" + now.getSeconds());
+	let time = document.querySelector('.time');
+
+	time.textContent = timeNow;
+}
+
+const getDate = () => {
+	let now = new Date(),
+		day = (now.getDate() > 10 ? now.getDate() : "0" + now.getDate()),
+		month = (now.getMonth() > 8 ? (now.getMonth() + 1) : "0" + (now.getMonth() + 1)),
+		year = now.getFullYear(),
+		dateNow = day + "." + month + "." + year;
+	let date = document.querySelector('.date');
+	date.textContent = dateNow;
+}
+
+const getQuote = async () => {
+	const response = await fetch(quoteURL);
+	if (response.ok) {
+		return await response.json();
+	}
+	else {
+		return response.status;
+	}
+}
+
+const changeQuote = () => {
+	const quoteArray = getQuote();
+	Promise.resolve(quoteArray).then(value => {
+		const randomQuote = getRandomIntInclusive(0, value.length)
+		const quoteText = document.querySelector('.quote-text-text');
+		const quoteTextAuthor = document.querySelector('.quote-text-author');
+
+		quoteText.textContent = value[randomQuote].text;
+		quoteTextAuthor.textContent = value[randomQuote].author;
+	});
+};
+
+// TODO refactor feat
+const userName = document.querySelector('.user-name');
+
+const saveUserName = () => {
+
+	localStorage.setItem('userName', userName.value);
+}
+
+const clearInput = () => {
+	userName.value = '';
+}
+
+const setUserName = () => {
+	if (localStorage.getItem('userName') == '')
+		localStorage.setItem('userName', userName.value);
+	else {
+		userName.value = localStorage.getItem('userName');
+	}
+}
+
+// TODO запросы погоды
+const getWeather = async () => {
+
+}
+
+const weather = getWeather();
+Promise.resolve(weather).then(value => {
+
+
+
+});
+// -------------------------------  events segment ------------------------------- 
+
+const btnChangeBG = document.querySelector('.btn-change-bg');
+const btnChangeQuote = document.querySelector('.btn-quote-change');
+
+userName.addEventListener('keyup', saveUserName);
+userName.addEventListener('click', clearInput);
+
+btnChangeBG.addEventListener('click', changeBackGround);
+btnChangeQuote.addEventListener('click', changeQuote);
+// -------------------------------  view segment ------------------------------- 
+
+document.body.style.backgroundSize = 'cover';
+document.body.style.backgroundPosition = 'center';
+document.body.style.backgroundRepeat = 'no-repeat';
+document.body.style.transition = 'background 1s';
+
+
+// -------------------------------  run function segment ------------------------------- 
+
+setUserName();
+changeQuote();
+getTime();
+getDate();
+changeBackGround();
+
+setInterval(changeQuote, 10000)
+setInterval(getTime, 100);
+setInterval(getDate, 5)
+setInterval(changeBackGround, 11000);
